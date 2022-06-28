72f43a002fe7e22c62d299129113bd51ad27ea79
Somnath Mishra <somutalks856@gmail.com>
2022-06-28 20:22:59 +0530

    hey

--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-// const {getJobsFromFreelanceCom, getJobsFromUpwork, getJobsFromPeoplePerHour} = require("../controllers/jobs")
-
-// router.get("/jobs/upwork", getJobsFromUpwork);
-// router.get("/jobs/freelancer", getJobsFromFreelanceCom);
-// router.get("/jobs/peopleperhour", getJobsFromPeoplePerHour);
-
 const {
   getCourseraCourses,
   getUdemyCourses,
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,11 +1,19 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
 
-const {getJobs, getJobsFromFreelanceCom, getJobsFromUpwork, getJobsFromPeoplePerHour} = require("../controllers/jobs")
+const {
+  getJobs,
+  getJobsFromFreelanceCom,
+  getJobsFromUpwork,
+  getJobsFromPeoplePerHour,
+} = require("../controllers/jobs");
 
-// router.get("/jobs/upwork", getJobsFromUpwork);
-// router.get("/jobs/freelancer", getJobsFromFreelanceCom);
-// router.get("/jobs/peopleperhour", getJobsFromPeoplePerHour);
-router.get("/jobs/", getJobsFromFreelanceCom, getJobsFromUpwork , getJobsFromPeoplePerHour, getJobs);
+router.get(
+  "/jobs/",
+  getJobsFromFreelanceCom,
+  getJobsFromUpwork,
+  getJobsFromPeoplePerHour,
+  getJobs
+);
 
 module.exports = router;
